c9a7640ec801f7288e8465f731b8aa829a531997
itaru-tok <itaruo93o@icloud.com>
2025-08-28 16:48:26 +0900

    make icon size bigger

--- wxt.config.ts
+++ wxt.config.ts
@@ -6,7 +6,14 @@ export default defineConfig({
     description: "Pin your tweet and jump back anytime on X",
     version: "1.0.1",
     permissions: ["storage"],
-    host_permissions: ["https://x.com/*", "https://twitter.com/*"]
+    host_permissions: ["https://x.com/*", "https://twitter.com/*"],
+    action: {
+      default_icon: {
+        "16": "icon-16.png",
+        "48": "icon-48.png",
+        "128": "icon-128.png"
+      }
+    }
   },
   srcDir: "src"
 });
