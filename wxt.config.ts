cfa125933f353bc242804171d8554c610e7a70e4
itaru-tok <itaruo93o@icloud.com>
2025-09-04 12:57:26 +0900

    update version

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   manifest: {
     name: "PinX",
     description: "Pin your tweet and jump back anytime on X",
-    version: "1.0.1",
+    version: "1.0.2",
     permissions: ["storage"],
     host_permissions: ["https://x.com/*", "https://twitter.com/*"],
     action: {
